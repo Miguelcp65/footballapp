83ceccf387faf1fc5c9b8036f5d382ada0f4b345
65vint <miguel.ciencia655@gmail.com>
2023-04-06 08:53:11 +0100

    Update FootballNews.jsx

--- components/home/footballnews/FootballNews.jsx
+++ components/home/footballnews/FootballNews.jsx
@@ -21,9 +21,6 @@ const FootballNews = () => {
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.headerTitle}>Football News</Text>
-        <TouchableOpacity>
-          <Text style={styles.headerBtn}>Show all</Text>
-        </TouchableOpacity>
       </View>
 
       <View style={styles.cardsContainer}>
